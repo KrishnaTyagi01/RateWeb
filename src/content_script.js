7341fd0271d948aca860c76e61467faded0f411e
KrishnaTyagi01 <tyagikrishna38@gmail.com>
2022-11-03 18:08:15 +0530

    add comment UI

--- src/content_script.js
+++ src/content_script.js
@@ -82,28 +82,67 @@ function closeDialog() {
 }
 
 
-
-
 function showIcon() {
     var body = document.getElementsByTagName("body")[0];
-    // var icondiv = document.createElement('div'); 
-    // icondiv.style.cssText = "font-size: 2rem;position: fixed;z-index:10; bottom: 1rem; right: 1rem;" 
-    // icondiv.textContent = `My Icon`
+    
     var logobtn = document.createElement('img');
     logobtn.src = chrome.runtime.getURL("assets/logo.svg");
     logobtn.setAttribute("id","getweblogobtn");
     logobtn.addEventListener('mouseover', openDialog);
-    // logobtn.addEventListener('', closeDialog);
     logobtn.style.cssText = "width:4rem; height:4rem; position: fixed;z-index:10000; bottom: 2rem; right: 2rem;cursor: pointer;";
    
     var reviewBox = document.createElement('div');
     reviewBox.classList.add('reviewbox');
     reviewBox.setAttribute("id","reviewbox");
-    reviewBox.style.cssText = 'height:50vh; width:30vw; visibility: hidden; display:fixed; background:red;position:fixed;z-index:10000; bottom:5.5rem; right:5.5rem';
-   
-   
+    reviewBox.style.cssText =
+     "height:50vh; width:30vw;background-color:#202124; visibility: hidden;position:fixed;z-index:10000; bottom:5.5rem; right:5.5rem;box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px; display:flex; flex-direction:column; ";
 
-    // I'm using "click" but it works with any event
+
+    var reviewHeader = document.createElement('div');
+    reviewHeader.classList.add('reviewheader');
+    reviewHeader.textContent = "comments(9)";
+    reviewHeader.style.cssText = 
+    'height:15%;background-color:#2D2F30;font-weight: 500;font-size: 1.5rem;padding:1rem;color: #FFFFFF;display:flex; align-items:center;'
+
+    var reviewbody = document.createElement('div');
+    reviewbody.classList.add('reviewbody');
+    reviewbody.style.cssText = 'height:70%;background-color:#202124;overflow-y: auto'
+
+    // ============= COMMENT START ================= //
+
+    var commentDiv = document.createElement('div');
+    commentDiv.style.cssText = 'border-bottom: 1px solid #2D2F31; padding:1rem;'
+
+    var userName = document.createElement('h3');
+    userName.textContent = 'Krishna Tyagi';
+    userName.style.cssText = 'font-weight:400;font-size:16px;color: #FFFFFF;'
+
+    var comment = document.createElement('p');
+    comment.textContent = 'This is a comment sample. Users with Ditch the SEO extension can comment on the links and help the community to search better..'
+    comment.style.cssText = 'font-weight: 400;font-size: 14px;color: #FFFFFF;opacity: 0.5;letter-spacing:1px;'
+
+    var addComment = document.createElement('input');
+    addComment.classList.add('addcomment');
+    addComment.placeholder = "Add Your Comment here"
+    addComment.style.cssText =
+   'height:15%;background-color:#202124;padding:1rem;font-size: 14px;opacity: 0.5;font-weight: 400;color: #FFFFFF;box-shadow: 0px -5px 25px 7px rgba(0, 0, 0, 0.5);border-radius: 4px 4px 0px 0px;border-top: 1px solid #2D2F30;'
+
+    commentDiv.appendChild(userName)
+    commentDiv.appendChild(comment)
+
+ // ============= COMMENT END ================= //
+ reviewbody.appendChild(commentDiv);
+    for(let i=0; i<2; i++){
+      var cloned = commentDiv.cloneNode(true);
+      reviewbody.appendChild(cloned);
+    }
+
+    reviewBox.appendChild(reviewHeader)
+    reviewBox.appendChild(reviewbody)
+    reviewBox.appendChild(addComment)
+
+  
+    
     document.addEventListener('click', event => {
       const isClickInside = reviewBox.contains(event.target)
     
@@ -117,9 +156,7 @@ function showIcon() {
    
     body.appendChild(logobtn);
     body.appendChild(reviewBox);
-   
-
-
+  
 }
 
 window.onload = function () {
